f1ae40b0cc1b5e17d607f371cc9095e251b616c3
WCIC <1181347984@qqq.com>
2024-09-21 15:50:46 +0800

    day1

---
+++ day1/01Hello/hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1;
+
+	cout << "a = " << a << endl;
+
+	cout << a << endl;
+
+	cout << "hello" << endl;
+	
+	system("pause");
+
+	return 0;
+
+}
+
